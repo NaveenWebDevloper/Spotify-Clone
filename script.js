b88b2aa66d55143a71380c20e987ffabcbe98afe
Naveen <NaveenWebDev14@gmail.com>
2024-12-09 00:47:03 +0530

    changes added
    smooth scroll added and included Sheryjs

---
+++ script.js
@@ -0,0 +1,11 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true
+});
+
+Shery.mouseFollower();
+Shery.makeMagnet(".Spotifylogo");
+Shery.makeMagnet(".sign")
+
+Shery.makeMagnet(".playlis");
+Shery.makeMagnet(".leftbar");
